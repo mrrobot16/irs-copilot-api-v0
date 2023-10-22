d736fd9ffbca09cb99ce26f26619a398da85975c
Hector G <hmgarcia06@gmail.com>
2023-10-22 19:42:00 -0400

    Added new models for nested models

--- db/health_model.py
+++ db/health_model.py
@@ -1,22 +1,47 @@
-from typing import Union
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, HttpUrl
 from enum import Enum
-
 
 class HealthEnum(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+class HealthImageModel(BaseModel):
+    url: HttpUrl
+    name: str
+
 class HealthItemModel(BaseModel):
     name: str
     description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
+        default=None, title="The description of the item", max_length=300,
+        examples=["A very nice Item example", "Some other description"]
     )
     price: float = Field(gt=0, description="The price must be greater than zero")
     tax: float | None = None
     is_offer: bool | None = None
     tags: list[str] = []
+    images: list[HealthImageModel] | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ],
+            "description": "A detailed description of the ItemModel.",
+            "title": "HealthItem Title"
+        }
+    }
+
+class HealthOfferModel(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[HealthItemModel]
 
 class HealthUserModel(BaseModel):
     username: str
