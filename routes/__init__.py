39187b00686a1ac872390b5a346a2226b7a3e399
Hector G <hmgarcia06@gmail.com>
2023-10-22 18:27:30 -0400

    Implemented post router to routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routers.app_health_get import app_health_get
 from routers.app_health_put import app_health_put
+from routers.app_health_post import app_health_post
 
 def configure_routes(app: FastAPI):
     app.include_router(app_health_get, prefix="/health-get", tags=["Health GET"])
     app.include_router(app_health_put, prefix="/health-put", tags=["Health PUT"])
+    app.include_router(app_health_post, prefix="/health-post", tags=["Health POST"])
