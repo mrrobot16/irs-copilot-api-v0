8a6b05d5fc675c807cbab4dce5b00381837db13e
Hector G <hmgarcia06@gmail.com>
2023-10-24 21:29:52 -0400

    Commented rest of routers for testing

--- routes/__init__.py
+++ routes/__init__.py
@@ -7,12 +7,14 @@ from routers.app_extra_router import app_extra_router
 from routers.app_form_data import app_form_data_router
 from routers.app_error_handling import app_error_handling_router
 from routers.app_dependency_injection import app_dependency_injection
+from routers.app_security import app_securiy
 
 def configure_routes(app: FastAPI):
-    app.include_router(app_dependency_injection, prefix="/dependency-injection", tags=["Dependency Injection"])
-    app.include_router(app_error_handling_router, prefix="/error-handling", tags=["Error Handling"])    
-    app.include_router(app_form_data_router, prefix="/form-data", tags=["Form Data"])
-    app.include_router(app_extra_router, prefix="/extra", tags=["Extra"])
-    app.include_router(app_health_get, prefix="/health-get", tags=["Health GET"])
-    app.include_router(app_health_put, prefix="/health-put", tags=["Health PUT"])
-    app.include_router(app_health_post, prefix="/health-post", tags=["Health POST"])
+    app.include_router(app_securiy, prefix="/auth", tags=["Auth"])
+    # app.include_router(app_dependency_injection, prefix="/dependency-injection", tags=["Dependency Injection"])
+    # app.include_router(app_error_handling_router, prefix="/error-handling", tags=["Error Handling"])    
+    # app.include_router(app_form_data_router, prefix="/form-data", tags=["Form Data"])
+    # app.include_router(app_extra_router, prefix="/extra", tags=["Extra"])
+    # app.include_router(app_health_get, prefix="/health-get", tags=["Health GET"])
+    # app.include_router(app_health_put, prefix="/health-put", tags=["Health PUT"])
+    # app.include_router(app_health_post, prefix="/health-post", tags=["Health POST"])
