5e32a6c6cfe63933632cad8676094826c1a5e4ce
Hector G <hmgarcia06@gmail.com>
2023-10-24 21:56:01 -0400

    Added auth methods and utilities for auth endpoint

--- routers/app_security.py
+++ routers/app_security.py
@@ -1,24 +1,38 @@
+from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Annotated
 from fastapi.security import OAuth2PasswordRequestForm
 
-from db.security import User, get_current_user, fake_users_db, UserInDB, fake_hash_password
+from db.security import User, get_current_active_user, fake_users_db, authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, Token, create_access_token
 
+app_security = APIRouter()
 
-app_securiy = APIRouter()
+@app_security.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
-@app_securiy.get("/users/me")
-async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]):
+@app_security.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
     return current_user
 
-@app_securiy.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    return {"access_token": user.username, "token_type": "bearer"}
+@app_security.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
