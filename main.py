19dfcbdacef73256b94f2df4360be1c36abc348a
Hector G <hmgarcia06@gmail.com>
2023-10-25 19:26:12 -0400

    Added middleware function and implemented CORS

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Depends, status
 from routes import configure_routes
 from routes.auth import configure_auth_routes
 from error_handlers import configure_error_handlers
+from middleware import configure_middleware, origins
 # from services import verify_token, verify_key
 # NOTE: This will enforce that every single request need token and key.
 # app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
@@ -13,5 +14,6 @@ async def health():
     return {"status": status.HTTP_200_OK}
 
 configure_error_handlers(app)
+configure_middleware(app)
 configure_auth_routes(app)
 configure_routes(app)
---
+++ middleware/__init__.py
@@ -0,0 +1,28 @@
+import time
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000"
+]
+
+def configure_middleware(app: FastAPI):
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    @app.middleware("http")
+    async def add_process_time_header(request: Request, call_next):
+        print(f"Request: {request.method} {request.url.path}")
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+        return response
