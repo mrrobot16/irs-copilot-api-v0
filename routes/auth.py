5c089c37aebeb9e1c7ab3e6f2033a974bd04ae05
Hector G <hmgarcia06@gmail.com>
2023-10-24 22:42:13 -0400

    Updated routes

---
+++ routes/auth.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+from fastapi import FastAPI, Depends, HTTPException, status
+from typing import Annotated
+
+from fastapi.security import OAuth2PasswordRequestForm
+
+from db.auth import User, Token, get_current_active_user, fake_users_db, authenticate_user, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+
+def configure_auth_routes(app: FastAPI):
+    @app.post("/token", response_model=Token)
+    async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+    ):
+        user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        return {"access_token": access_token, "token_type": "bearer"}
+    
+    @app.get("/users/me/", response_model=User)
+    async def read_users_me(
+        current_user: Annotated[User, Depends(get_current_active_user)]
+    ):
+        return current_user
+
+    @app.get("/users/me/items/")
+    async def read_own_items(
+        current_user: Annotated[User, Depends(get_current_active_user)]
+    ):
+        return [{"item_id": "Foo", "owner": current_user.username}]
