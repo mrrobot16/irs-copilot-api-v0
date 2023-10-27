45e76c80fb115c3b31401ef48ef40bda9394d448
Hector G <hmgarcia06@gmail.com>
2023-10-26 20:51:15 -0400

    Added metadata to the docs

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI, Depends, status
+from fastapi.staticfiles import StaticFiles
 
 from routes import configure_routes
 from routes.auth import configure_auth_routes
 from error_handlers import configure_error_handlers
-from middleware import configure_middleware, origins
-# from services import verify_token, verify_key
-# NOTE: This will enforce that every single request need token and key.
-# app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
-app = FastAPI()
+from middleware import configure_middleware
+from metadata import metadata
+from services import verify_token, verify_key
+
+app = FastAPI(
+    title = metadata["title"],
+    description = metadata["description"],
+    summary = metadata["summary"],
+    version = metadata["version"],
+    terms_of_service = metadata["terms_of_service"],
+    contact = metadata["contact"],
+    license_info = metadata["license_info"],
+    openapi_tags = metadata["tags"],
+    openapi_url = metadata["openapi_url"],
+    dependencies=[Depends(verify_token), Depends(verify_key)], # NOTE: This will enforce that every single request need token and key.
+    docs_url="/docs", 
+    redoc_url="/documentation"
+)
 
 @app.get("/")
 async def health():
---
+++ metadata.py
@@ -0,0 +1,32 @@
+metadata = {
+    "title": "FastAPI Tutorial",
+    "version": "0.0.1",
+    "description": "FastAPI helps you do awesome stuff. 🚀",
+    "summary": "Deadpool's favorite app. Nuff said.",
+    "contact": {
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    "license_info": {
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        "identifier": "MIT",
+    },
+    "terms_of_service": "http://example.com/terms/",
+    "tags": [
+        {
+            "name": "users",
+            "description": "Operations with users. The **login** logic is also here.",
+        },
+        {
+            "name": "items",
+            "description": "Manage items. So _fancy_ they have their own docs.",
+            "externalDocs": {
+                "description": "Items external docs",
+                "url": "https://fastapi.tiangolo.com/",
+            },
+        },
+    ],
+    "openapi_url": "/api/v1/openapi.json",
+}
