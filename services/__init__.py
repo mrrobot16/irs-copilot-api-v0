9c24cc4fbf4b33cf132ab19749bf44efa077d262
Hector G <hmgarcia06@gmail.com>
2023-10-26 20:52:19 -0400

    Commented out the logic that requires user to set x-token and x-key on every request

--- services/__init__.py
+++ services/__init__.py
@@ -2,14 +2,16 @@ from typing import Annotated
 
 from fastapi import Depends, Cookie, HTTPException, Header
 
-async def verify_token(x_token: Annotated[str, Header()]):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
+async def verify_token(x_token: Annotated[str, Header()] = None):
+    pass
+    # if x_token != "fake-super-secret-token":
+    #     raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-async def verify_key(x_key: Annotated[str, Header()]):
-    if x_key != "fake-super-secret-key":
-        raise HTTPException(status_code=400, detail="X-Key header invalid")
+async def verify_key(x_key: Annotated[str, Header()] = None):
+    pass
+    # if x_key != "fake-super-secret-key":
+    #     raise HTTPException(status_code=400, detail="X-Key header invalid")
 
 
 async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
